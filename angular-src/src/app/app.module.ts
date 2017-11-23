ca83e00aaa353fec6d5fab119130777b53b7bbc6
Tim Veestraeten <r0613758@student.thomasmore.be>
2017-11-23 17:13:52 +0100

    visuele weergaven reservaties, gebruikt nog dummiedata

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -15,12 +15,16 @@ import {ValidateService} from './services/validate.service';
 import {AuthService} from './services/auth.service';
 import{FlashMessagesModule} from'angular2-flash-messages';
 import {AuthGuard} from './guards/auth.guard';
+import { TimelineComponent } from './components/timeline/timeline.component';
+import { TimelinesComponent } from './components/timelines/timelines.component';
+import { WeekViewComponent } from './components/week-view/week-view.component';
 
 const appRoutes:Routes=[
 
   {path:'',component:HomeComponent},
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
+  {path:'timeline',component:TimelinesComponent},
   {path:'dashboard',component:DashboardComponent,canActivate:[AuthGuard]},
   {path:'profile',component:ProfileComponent,canActivate:[AuthGuard]},
 ]
@@ -32,7 +36,10 @@ const appRoutes:Routes=[
     RegisterComponent,
     HomeComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    TimelineComponent,
+    TimelinesComponent,
+    WeekViewComponent
   ],
   imports: [
     BrowserModule,
