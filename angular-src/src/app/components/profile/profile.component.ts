7293812e741c3477c1c812c28b91ebbe58b72621
JordyLuckermans <jordyluckermans@gmail.com>
2017-11-23 10:18:52 +0100

    angular-src
    it works! hallelujah!!!

---
+++ angular-src/src/app/components/profile/profile.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import{AuthService} from'../../services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+user:Object;
+  filesToUpload: Array<File>;
+  constructor(private authService:AuthService,private router:Router) {
+    this.filesToUpload = [];
+  }
+
+  ngOnInit() {
+    this.authService.getProfile().subscribe(profile=> {
+      this.user = profile.user;
+    },
+      err=>{
+      console.log(err);
+      return false;
+    });
+  }
+
+  upload() {
+    this.makeFileRequest("http://localhost:6600/upload", [], this.filesToUpload).then((result) => {
+      console.log(result);
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>> fileInput.target.files;
+  }
+
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
+    return new Promise((resolve, reject) => {
+      var formData: any = new FormData();
+      var xhr = new XMLHttpRequest();
+      for(var i = 0; i < files.length; i++) {
+        formData.append("uploads[]", files[i], files[i].name);
+      }
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      }
+      xhr.open("POST", url, true);
+      xhr.send(formData);
+    });
+  }
+
+}
