f9ece856f22d80ed9064529b03fff92ed83fbe8e
Jonas <jonassaet@gmail.com>
2017-11-24 13:49:04 +0100

    form voor reserve

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import{AuthService} from '../../services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {FlashMessagesService} from 'angular2-flash-messages';
 
@@ -9,37 +9,37 @@ import {FlashMessagesService} from 'angular2-flash-messages';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-username:String;
-password:String;
+  username: String;
+  password: String;
 
-  constructor(private authService:AuthService,
-  private router:Router,
-private flashMessage:FlashMessagesService
-) { }
+  constructor(private authService: AuthService,
+              private router: Router,
+              private flashMessage: FlashMessagesService) {
+  }
 
   ngOnInit() {
   }
 
-  onLoginSubmit(){
-const user={
-  username: this.username,
-  password:this.password
-}
-this.authService.authenticateUser(user).subscribe(data =>{
-if(data.success){
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+    this.authService.authenticateUser(user).subscribe(data => {
+      if (data.success) {
 
-this.authService.storeUserData(data.token,data.user);
-  this.flashMessage.show('You are now logged in!',{cssClass:'alert-success',timeout:5000});
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessage.show('U bent nu aangemeld! ', {cssClass: 'alert-success', timeout: 5000});
 
-  this.router.navigate(['dashboard']);
+        this.router.navigate(['dashboard']);
 
-}else{
-  this.flashMessage.show(data.msg,{cssClass:'alert-danger',timeout:5000});
+      } else {
+        this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 5000});
 
-  this.router.navigate(['login']);
-}
+        this.router.navigate(['login']);
+      }
 
-});
+    });
   }
 
 }
