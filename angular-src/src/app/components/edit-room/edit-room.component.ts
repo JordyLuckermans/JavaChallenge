cc9c361518bc2bbbf974ffbe248202ba5530bdf7
MarijkeM <marijkemeersman@gmail.com>
2017-11-25 15:58:11 +0100

    service geïntegreerd in editroom

--- angular-src/src/app/components/edit-room/edit-room.component.ts
+++ angular-src/src/app/components/edit-room/edit-room.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {RoomService} from '../../services/room.service';
 
 @Component({
   selector: 'app-edit-room',
   templateUrl: 'edit-room.component.html',
   styleUrls: ['edit-room.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  providers:[RoomService]
 })
 export class EditRoomComponent implements OnInit {
+  rooms:Room[];
 
-  constructor() { }
+  constructor(private roomService:RoomService) {
+    this.roomService.getRooms().subscribe(rooms=> {
+      console.log(rooms);
+    })
+  }
 
   ngOnInit() {
   }
 
+
+}
+interface Room{
+  id: number;
+  name:string;
+  location:String;
+  description:String;
 }
