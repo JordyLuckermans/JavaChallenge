89a2770be66e9cd9b0864fa5e6fe7bbcf0dafa34
Tim Veestraeten <r0613758@student.thomasmore.be>
2017-11-25 13:15:12 +0100

    dummiedata zalen naar mongo schrijven

---
+++ angular-src/src/app/components/dummie-data/dummie-data.component.ts
@@ -0,0 +1,94 @@
+import {Component, OnInit} from '@angular/core';
+import {RoomService} from "../../services/room.service";
+import {Room} from "../../models/room.model";
+
+@Component({
+  selector: 'app-dummie-data',
+  templateUrl: './dummie-data.component.html'
+})
+export class DummieDataComponent implements OnInit {
+
+  constructor(private roomService: RoomService) { }
+
+  ngOnInit() {
+  }
+
+  generateRooms() {
+    const rooms: Room[] = [
+      {
+        name: "mediaruimte",
+        dimensions: "39.2m²",
+        location: "urban center",
+        description: "heeft media"
+      },
+      {
+        name: "akoestische ruimte",
+        dimensions: "36.3m²",
+        location: "urban center",
+        description: "heeft goede akoestiek"
+      },
+      {
+        name: "studio",
+        dimensions: "39.2m²",
+        location: "urban center",
+        description: "lege description geeft error"
+      },
+      {
+        name: "bar/lounge",
+        dimensions: "199.2m²",
+        location: "urban center",
+        description: "Mensen moeten door deze ruimte als ze naar een andere zaal willen"
+      },
+      {
+        name: "grote danszaal 1",
+        dimensions: "groot",
+        location: "urban center",
+        description: "Vormt feestzaal samen met grote  danszalen 2 en 3."
+      },
+      {
+        name: "grote danszaal 2",
+        dimensions: "groot",
+        location: "urban center",
+        description: "Vormt feestzaal samen met grote  danszalen 1 en 3."
+      },
+      {
+        name: "grote danszaal 3",
+        dimensions: "groot",
+        location: "urban center",
+        description: "Vormt feestzaal samen met grote  danszalen 1 en 2."
+      },
+      {
+        name: "kleine danszaal 1",
+        dimensions: "79.6m²",
+        location: "urban center",
+        description: "Heeft een spiegel."
+      },
+      {
+        name: "kleine danszaal 2",
+        dimensions: "94.4m²",
+        location: "urban center",
+        description: "Heeft een spiegel."
+      },
+      {
+        name: "kleine danszaal 3",
+        dimensions: "115.8m²",
+        location: "urban center",
+        description: "Heeft een spiegel."
+      },
+      {
+        name: "vergaderzaal",
+        dimensions: "heel groot",
+        location: "urban center",
+        description: "Is zo groot als de feestzaal."
+      }
+    ];
+
+    rooms.forEach((room: Room) => {
+      this.roomService.addRoom(room).subscribe(
+        res => console.log(res),
+        err => console.error(err)
+      );
+    });
+  }
+
+}
