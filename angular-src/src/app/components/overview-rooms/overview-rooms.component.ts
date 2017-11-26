2b67122bd1f75af8d37d3e33bccb6b2f7212ad2b
MarijkeM <marijkemeersman@gmail.com>
2017-11-26 17:43:39 +0100

    rooms worden getoond

--- angular-src/src/app/components/overview-rooms/overview-rooms.component.ts
+++ angular-src/src/app/components/overview-rooms/overview-rooms.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 import {RoomService} from '../../services/room.service';
+import {Room} from "../../models/room.model";
 
 @Component({
-  selector: 'app-overview-rooms',
-  templateUrl: 'overview-rooms.component.html',
-  styleUrls: ['overview-rooms.component.css'],
-  encapsulation: ViewEncapsulation.None,
-  providers:[RoomService]
+    selector: 'app-overview-rooms',
+    /*templateUrl: 'overview-rooms.component.html',*/
+    template: `
+        <div class="col-md-8 col-md-offset-2">
+        <p>Hallo</p>
+            <app-room [room]=room *ngFor="let room of rooms">
+            </app-room>
+        </div>
+`,
+    encapsulation: ViewEncapsulation.None
 })
 export class OverviewRoomsComponent implements OnInit {
+    rooms: Room[];
 
-  constructor() { }
+    constructor(private roomService: RoomService) {
+        console.log("constructor done");
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        console.log("ngOnInit done");
+        this.roomService.getRooms().subscribe(
+            (rooms: Room[]) => {
+                this.rooms = rooms;
+            }
+            //res=>console.log(this.roomService.rooms),
+            //err=>console.error(err),
+        );
+    }
 
 }
