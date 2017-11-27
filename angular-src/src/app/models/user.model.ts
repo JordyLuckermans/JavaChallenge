5ec0a92feccf455b64eccab1b7751a48f0df91c5
MarijkeM <marijkemeersman@gmail.com>
2017-11-27 01:54:45 +0100

    dummy users kan je toevoegen

---
+++ angular-src/src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public name:String,
+        public username:String,
+        public email:String,
+        public password:String,
+        public phoneNumber:String,
+        public isAdmin:Boolean,
+    ){}
+}
